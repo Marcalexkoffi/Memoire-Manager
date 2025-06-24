480f118a246ad51f02e1e86ad1ac57eb2d687b53
Koffi Marc Alex <koffimarcalex6@gmail.com>
2025-06-24 18:25:03 +0000

    Premier commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,44 @@
+import { RegisterComponent } from './register/register.component';
+import { StudentSubmitComponent } from './student-submit/student-submit.component';
+import { StudentDashboadComponent } from './student-dashboad/student-dashboad.component';
+import { ProfessorDashboadComponent } from './professor-dashboad/professor-dashboad.component';
 import { Routes } from '@angular/router';
-import path from 'path';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'professor-dashboard',
+    component: ProfessorDashboadComponent,
+  },
+  {
+    path: 'student-dashboard',
+    component: StudentDashboadComponent,
+  },
+  {
+    path: 'student-submit',
+    component: StudentSubmitComponent,
+  },
+];
