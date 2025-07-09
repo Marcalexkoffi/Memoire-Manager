b46e629b675860861c60b833e960458f42d3c1ad
Koffi Marc Alex <koffimarcalex6@gmail.com>
2025-07-09 10:24:12 +0000

    modification du formulaire de mémore

--- Front/src/app/student-submit/student-submit.component.ts
+++ Front/src/app/student-submit/student-submit.component.ts
@@ -10,9 +10,7 @@ import { CommonModule } from '@angular/common';
 import { AuthenticationService } from '../services/authentication.service';
 
 export interface StudentSubmission {
-  contact: string;
   domaine: string;
-  fullName: string;
   themeTitle: string;
   problematic: string;
   generalObjective: string;
@@ -43,9 +41,7 @@ export class StudentSubmitComponent implements OnInit {
 
   initializeForm(): void {
     this.studentForm = this.formBuilder.group({
-      contact: ['', [Validators.required]], // Type text maintenant
       domaine: ['', [Validators.required]],
-      fullName: ['', [Validators.required]],
       themeTitle: ['', [Validators.required]],
       problematic: ['', [Validators.required]],
       generalObjective: ['', [Validators.required]],
@@ -63,9 +59,8 @@ export class StudentSubmitComponent implements OnInit {
       setTimeout(() => {
         const formValue = this.studentForm.value;
         this.submittedData = {
-          contact: formValue.contact,
           domaine: formValue.domaine,
-          fullName: formValue.fullName,
+
           themeTitle: formValue.themeTitle,
           problematic: formValue.problematic,
           generalObjective: formValue.generalObjective,
